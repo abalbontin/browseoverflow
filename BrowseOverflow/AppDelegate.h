81c2e7522d0b6b99f175b4468ce8eca152f7dec4
abalbontin <alvaro@mobivery.com>
2013-03-24 20:50:20 +0100

    Preparación inicial del proyecto tal y como indican en el capitulo 5.

---
+++ BrowseOverflow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrowseOverflow
+//
+//  Created by Álvaro Balbontín Gutiérrez on 24/03/13.
+//  Copyright (c) 2013 Álvaro Balbontín Gutiérrez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
