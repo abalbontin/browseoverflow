ed124dece22b894c71b5cbb0292fa2e17cdafd83
abalbontin <alvaro@mmip.es>
2013-03-27 17:54:20 +0100

    Creada la clase Person y los teste para comprobar que tiene nombre y la url del avatar.

---
+++ BrowseOverflowTests/PersonTests.h
@@ -0,0 +1,17 @@
+//
+//  PersonTests.h
+//  BrowseOverflow
+//
+//  Created by MBV15 on 27/03/13.
+//  Copyright (c) 2013 Álvaro Balbontín Gutiérrez. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@class Person;
+
+@interface PersonTests : SenTestCase
+
+@property Person *person;
+
+@end
